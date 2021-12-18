c155903a226bbb3106d00bac8c06f68a9de3c4ab
IonutParau <ionut.alex.parau@gmail.com>
2021-12-18 13:13:25 +0200

    Fixed title bug
    Sorry, it still said CelLuAPI in the title, I fixed that now

--- conf.lua
+++ conf.lua
@@ -3,12 +3,12 @@ require 'api.config'
 function love.conf(t)
 	local config = loadConfig()
 	t.console = (config['debug'] == 'true')
-	t.identity = "CelLuAPI"
+	t.identity = "CelLua"
 	t.window.width = 800
 	t.window.height = 600
 	t.window.resizable = true
 	t.window.icon = "icon.png"
-	t.window.title = "CelLuAPI Machine"
+	t.window.title = "CelLua Machine"
 	t.modules.physics = false
 	t.modules.video = false
 	t.modules.thread = false
